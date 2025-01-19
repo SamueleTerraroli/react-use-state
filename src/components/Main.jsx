4377b17a747e8c31e569e7fb017a936c03ea9e53
SamueleTerraroli <terraroli99@gmail.com>
2025-01-19 10:55:04 +0100

    feat: add structure

---
+++ src/components/Main.jsx
@@ -0,0 +1,20 @@
+const Main = () => {
+    return (
+        <>
+            <section id="buttons" className="d-flex justify-content-center">
+                <button>button</button>
+            </section>
+
+            <section id="description" className="d-flex justify-content-center col-5 ">
+                <div>
+                    <h4>titolo</h4>
+                    <p>descrizione</p>
+
+                </div>
+
+            </section>
+        </>
+    )
+}
+
+export default Main
